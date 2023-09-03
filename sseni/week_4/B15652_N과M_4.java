b4005fcaca78cf0a32860609fbe1220fc83650b9
JISEON LEE <55522275+jsl1113@users.noreply.github.com>
2023-09-03 15:09:05 +0900

    Update B15652_N과M_4.java

--- sseni/week_4/B15652_N과M_4.java
+++ sseni/week_4/B15652_N과M_4.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+// n과 m 5,6은 오히려 금방 풀었는데 의외로 4가 원하는대로 답이 잘 나오지 않았다.
+// 이전의 코드를 참고해서 풀었다.
+// 다해놓고 헤맸던 것이었다. 40 line 에서 i 가 아니라 idx 를 넣고 계속 돌렸다.
+// 이것도 재귀를 제대로 이해 못했다는 것이겠지 ㅜ
 public class B15652 {
     static int n, m;
     static StringBuilder sb;
