25c5e90e7040916e34bc6a012ff400964604c419
sowowowos <aceannie@naver.com>
2023-09-03 17:27:57 +0900

    [MOD] week_4 B2961 도영이가 만든 맛있는 음식

--- somin/week_4/B2961_도영이가만든맛있는음식.java
+++ somin/week_4/B2961_도영이가만든맛있는음식.java
@@ -14,10 +14,10 @@ public class B2961_도영이가만든맛있는음식 {
 	static int sum = Integer.MAX_VALUE;
 	
 	public static class Point{
-		int x,y;
-		Point(int x,int y){
-			this.x = x;
-			this.y = y;
+		int s,b;
+		Point(int s,int b){
+			this.s = s;
+			this.b = b;
 		}
 	}
 	public static void main(String[] args) throws IOException {
@@ -36,20 +36,18 @@ public class B2961_도영이가만든맛있는음식 {
 	
 	public static void dfs(int cnt, int start) {
 		if(cnt==n) {
-			//ck : isVisited가 모두 false인 경우에는 요리의 맛 차이를 구하지 않기 위해 체크
+			//ck : isVisited가 모두 false인 경우에는 요리의 맛 차이를 구하지 않기 위해 체크 ->없앰
 			//신맛은 곱셈이여서 1로 초기화, 쓴맛은 덧셈이므로 0 초기화
-			int ck=0;
 			int sour = 1;
 			int bitter =0;
 			
 			for(int i=0;i<n;i++) {
 				if(isVisited[i]) {
-					ck++;
-					sour = sour * ingredient.get(i).x;
-					bitter = bitter + ingredient.get(i).y;
+					sour = sour * ingredient.get(i).s;
+					bitter = bitter + ingredient.get(i).b;
 				}
 			}
-			if(ck>0) {
+			if(sour!=1 && bitter!=0) {
 				sum = Math.min(sum, Math.abs(sour-bitter));				
 			}
 			return;
