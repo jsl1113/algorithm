f9a420fa4070b5fbaacbced9916597883e941118
jsl1113 <jsl1113@naver.com>
2023-12-28 13:32:08 +0900

    [ADD] B11048

---
+++ sseni/week_20/B11048_이동하기.java
@@ -0,0 +1,37 @@
+package com.example.algorithm.study.week20;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B11048_이동하기 {
+    static int n, m, arr[][];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] += Math.max(arr[i - 1][j - 1], Math.max(arr[i - 1][j], arr[i][j - 1]));
+            }
+        }
+
+        bw.write(String.valueOf(arr[n][m]));
+        bw.flush();
+        bw.close();
+    }
+}
