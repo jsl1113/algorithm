afa2f64533f4ce38994b4639015af4452e0b4595
jsl1113 <jsl1113@naver.com>
2024-03-30 02:31:46 +0900

    [MOD] B18405

--- sseni/week_33/B18405_경쟁적전염.java
+++ sseni/week_33/B18405_경쟁적전염.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class B18405_경쟁적전염 {
     static int n, k, s, x, y;
     static int[][] arr;
+    static int[] dx = {0, 0, -1, 1}, dy = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,8 +26,8 @@ public class B18405_경쟁적전염 {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                if(arr[i][j] > 0) {
-                    queue[arr[i][j]].offer(new int[] {i, j, 0});
+                if (arr[i][j] > 0) {
+                    queue[arr[i][j]].offer(new int[]{i, j, 0});
                 }
             }
         }
@@ -35,22 +36,21 @@ public class B18405_경쟁적전염 {
         s = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken()) - 1;
         y = Integer.parseInt(st.nextToken()) - 1;
-        int[] dr = {-1,0,1,0}, dc = {0,-1,0,1};
 
         int time = 0;
-        while(time < s) {
+        while (time < s) {
             for (int i = 1; i <= k; i++) {
-                while(!queue[i].isEmpty()) {
-                    if(queue[i].peek()[2] == time) {
-                        int[] cur = queue[i].poll();
+                while (!queue[i].isEmpty()) {
+                    if (queue[i].peek()[2] == time) {
+                        int[] p = queue[i].poll();
                         for (int d = 0; d < 4; d++) {
-                            int nx = cur[0] + dr[d];
-                            int ny = cur[1] + dc[d];
+                            int nx = p[0] + dx[d];
+                            int ny = p[1] + dy[d];
 
-                            if(nx >= n || ny >= n || nx < 0 || ny < 0) continue;
-                            if(arr[nx][ny] > 0) continue;
+                            if (nx >= n || ny >= n || nx < 0 || ny < 0) continue;
+                            if (arr[nx][ny] > 0) continue;
                             arr[nx][ny] = i;
-                            queue[i].offer(new int[] {nx, ny, cur[2] + 1});
+                            queue[i].offer(new int[]{nx, ny, p[2] + 1});
                         }
                     } else break;
                 }
