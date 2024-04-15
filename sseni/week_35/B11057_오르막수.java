bff8069869dbfafade05827a26ef1aa94d2b1117
jsl1113 <jsl1113@naver.com>
2024-04-15 17:16:19 +0900

    [MOD] B11057

--- sseni/week_35/B11057_오르막수.java
+++ sseni/week_35/B11057_오르막수.java
@@ -5,14 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-/*
-자릿수에 따라,
-1자리 - 1 1 1 1 1 1 1 1 1 1 = 10
-2자리 - 10 9 8 7 6 5 4 3 2 1 = 55
-3자리 - 55 45 36 28 21 15 10 6 3 1 = 220
-dp[3][1] = dp[3][0] - dp[2][0]
-dp[3][2] = dp[3][1] - dp[2][1]
-*/
 public class B11057_오르막수 {
     static long MOD = 10_007;
 
@@ -27,26 +19,13 @@ public class B11057_오르막수 {
         }
 
         for (int i = 2; i <= n; i++) {
-            long sum = dp[i - 1][0];
-            for (int j = 1; j < 10; j++) {
-                sum += dp[i - 1][j] % MOD;
-            }
-            arr[i - 1] = sum % MOD;
-
             dp[i][9] = 1;
+
             for (int j = 8; j >= 0; j--) {
-                dp[i][j] = dp[i][j+1] % MOD + dp[i-1][j] % MOD;
-                dp[i][j] %= MOD;
+                dp[i][j] = (dp[i][j+1] + dp[i-1][j]) % MOD;
             }
         }
 
-        long sum = dp[n][0];
-        for (int j = 1; j < 10; j++) {
-            sum += dp[n][j] % MOD;
-        }
-        arr[n] = sum % MOD;
-
-
-        System.out.println(arr[n]);
+        System.out.println(Arrays.stream(dp[n]).sum() % MOD);
     }
 }
