c5eaa881cb5d87548ed0a1bdbafe782e905c8bd5
jsl1113 <jsl1113@naver.com>
2025-01-05 21:25:15 +0900

    [ADD] B21318

---
+++ sseni/week_69/B21318_피아노체조.java
@@ -0,0 +1,38 @@
+package com.example.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B21318_피아노체조 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] cnt = new int[n];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1]) {
+                cnt[i] = cnt[i - 1] + 1;
+            } else {
+                cnt[i] = cnt[i - 1];
+            }
+        }
+
+        int q = Integer.parseInt(br.readLine());
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+
+            if (x == y) System.out.println(0);
+            else System.out.println(cnt[y] - cnt[x]);
+        }
+    }
+}
