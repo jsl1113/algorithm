194166960bb98f00f880d9611b0ea00857e4a6dd
sowowowos <aceannie@naver.com>
2025-06-05 17:01:48 +0900

    [MOD] week_82

--- somin/week_82/B11568_민균이의계략.java
+++ somin/week_82/B11568_민균이의계략.java
@@ -22,7 +22,7 @@ public class B11568_민균이의계략 {
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 if(arr[j]<arr[i]){
-                    dp[i] = Math.max(dp[i], dp[i]+1);
+                    dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
             max = Math.max(max, dp[i]);
